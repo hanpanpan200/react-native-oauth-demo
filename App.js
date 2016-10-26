cdc40cfbbe55703ee647b1b5380873283f3a77ed
Grace Han <hanpanpan@Graces-MacBook-Pro.local>
2016-10-26 15:51:05 +0800

    feat: login/logout with LoginManager & LoginButton

--- App.js
+++ App.js
@@ -11,13 +11,31 @@ import {
   Text,
   View
 } from 'react-native';
-import { LoginManager } from 'react-native-fbsdk'
+import {
+  LoginManager,
+  LoginButton,
+  AccessToken,
+} from 'react-native-fbsdk'
 
 export default class App extends Component {
+  logoutFacebook = () => {
+    console.log('Logout with LoginManager========');
+    LoginManager.logOut()
+  }
+
   loginWithFacebook = () => {
+    console.log('Login with LoginManager>>>>>>>');
     LoginManager.logInWithReadPermissions(['public_profile']).then(
       function(result) {
-        console.log('result=====', result);
+        if (result.isCancelled) {
+          console.log("login is cancelled.");
+        } else {
+          AccessToken.getCurrentAccessToken().then(
+            (data) => {
+              console.log(data.accessToken.toString())
+            }
+          )
+        }
       }, function(error) {
         console.log('error=====', error.message);
       }
@@ -28,8 +46,31 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome} onPress={this.loginWithFacebook}>
-          Welcome to React Native!
+          Login with facebook via LoginManager
         </Text>
+        <Text style={styles.welcome} onPress={this.logoutFacebook}>
+          Logout facebook via LoginManager
+        </Text>
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              console.log('Login with Login Button>>>>>>>');
+              if (error) {
+                console.log("login has error: " + result.error);
+              } else if (result.isCancelled) {
+                console.log("login is cancelled.");
+              } else {
+                AccessToken.getCurrentAccessToken().then(
+                  (data) => {
+                    console.log(data.accessToken.toString())
+                  }
+                )
+              }
+            }
+          }
+          onLogoutFinished={() => console.log("Login out with Login Button.")}
+        />
       </View>
     );
   }
